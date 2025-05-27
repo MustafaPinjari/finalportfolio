ea3e4d2335faf4534f1e32ef13154c2ac1072c08
MUSTAFA <unlessuser99@gmail.com>
2025-05-27 12:22:35 +0530

    Fix deployment configuration for Render

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -o errexit
 # Use the deployment-specific requirements file
 pip install -r requirements-render.txt
 
+# Download required NLTK data for blog features
+python -m nltk.downloader punkt stopwords wordnet
+
 python manage.py collectstatic --no-input
 python manage.py migrate
